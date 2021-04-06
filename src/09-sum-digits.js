e42eede1c84419270b93c539547f7be994e8a8e5
Egor Borisevich <picassonk21@gmail.com>
2021-04-06 17:53:51 +0300

    feat: add solution of tasks 9 and 10

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,12 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  if (String(n).length > 1) {
+    const num = String(n).split('').map(Number).reduce(((acc, val) => acc + val), 0);
+    return getSumOfDigits(num);
+  }
+  return n;
 }
 
 module.exports = getSumOfDigits;
--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,13 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let i = 0;
+  while (i < array.length) {
+    if (array[i] === value) return i;
+    i++;
+  }
+  return -1;
 }
 
 module.exports = findIndex;
