f9379b49714d32c4c9893e82fd912e07f8a90117
Egor Borisevich <picassonk21@gmail.com>
2021-04-06 10:48:23 +0300

    fix: comment solution of 4 task to find error

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -23,93 +23,93 @@
  */
 
 function minesweeper(matrix) {
-  let copy = [];
-  matrix.forEach((arr) => {
-    arr.forEach((el) => {
-      copy.push(el);
-    });
-  });
-  copy = copy.map((el) => {
-    if (!el) {
-      return 0;
-    }
-    return el;
-  });
-  for (let i = 0; i < copy.length; i++) {
-    if (copy[i] === true) {
-      if (i % 3 === 0) {
-        if (copy[i - 3] !== undefined && typeof copy[i - 3] === 'number') {
-          copy[i - 3] += 1;
-        }
-        if (copy[i - 2] !== undefined && typeof copy[i - 2] === 'number') {
-          copy[i - 2] += 1;
-        }
-        if (copy[i + 1] !== undefined && typeof copy[i + 1] === 'number') {
-          copy[i + 1] += 1;
-        }
-        if (copy[i + 3] !== undefined && typeof copy[i + 3] === 'number') {
-          copy[i + 3] += 1;
-        }
-        if (copy[i + 4] !== undefined && typeof copy[i + 4] === 'number') {
-          copy[i + 4] += 1;
-        }
-      }
-      if (i % 3 === 1) {
-        if (copy[i - 4] !== undefined && typeof copy[i - 4] === 'number') {
-          copy[i - 4] += 1;
-        }
-        if (copy[i - 3] !== undefined && typeof copy[i - 3] === 'number') {
-          copy[i - 3] += 1;
-        }
-        if (copy[i - 2] !== undefined && typeof copy[i - 2] === 'number') {
-          copy[i - 2] += 1;
-        }
-        if (copy[i - 1] !== undefined && typeof copy[i - 1] === 'number') {
-          copy[i - 1] += 1;
-        }
-        if (copy[i + 1] !== undefined && typeof copy[i + 1] === 'number') {
-          copy[i + 1] += 1;
-        }
-        if (copy[i + 2] !== undefined && typeof copy[i + 2] === 'number') {
-          copy[i + 2] += 1;
-        }
-        if (copy[i + 3] !== undefined && typeof copy[i + 3] === 'number') {
-          copy[i + 3] += 1;
-        }
-        if (copy[i + 4] !== undefined && typeof copy[i + 4] === 'number') {
-          copy[i + 4] += 1;
-        }
-      }
-      if (i % 3 === 2) {
-        if (copy[i - 4] !== undefined && typeof copy[i - 4] === 'number') {
-          copy[i - 4] += 1;
-        }
-        if (copy[i - 3] !== undefined && typeof copy[i - 3] === 'number') {
-          copy[i - 3] += 1;
-        }
-        if (copy[i - 1] !== undefined && typeof copy[i - 1] === 'number') {
-          copy[i - 1] += 1;
-        }
-        if (copy[i + 2] !== undefined && typeof copy[i + 2] === 'number') {
-          copy[i + 2] += 1;
-        }
-        if (copy[i + 3] !== undefined && typeof copy[i + 3] === 'number') {
-          copy[i + 3] += 1;
-        }
-      }
-    }
-  }
-  copy = copy.map((el) => {
-    if (el === true) {
-      return 1;
-    }
-    return el;
-  });
-  const res = [];
-  for (let i = 0; i < copy.length; i += 3) {
-    res.push(copy.slice(i, i + 3));
-  }
-  return res;
+  // let copy = [];
+  // matrix.forEach((arr) => {
+  //   arr.forEach((el) => {
+  //     copy.push(el);
+  //   });
+  // });
+  // copy = copy.map((el) => {
+  //   if (!el) {
+  //     return 0;
+  //   }
+  //   return el;
+  // });
+  // for (let i = 0; i < copy.length; i++) {
+  //   if (copy[i] === true) {
+  //     if (i % 3 === 0) {
+  //       if (copy[i - 3] !== undefined && typeof copy[i - 3] === 'number') {
+  //         copy[i - 3] += 1;
+  //       }
+  //       if (copy[i - 2] !== undefined && typeof copy[i - 2] === 'number') {
+  //         copy[i - 2] += 1;
+  //       }
+  //       if (copy[i + 1] !== undefined && typeof copy[i + 1] === 'number') {
+  //         copy[i + 1] += 1;
+  //       }
+  //       if (copy[i + 3] !== undefined && typeof copy[i + 3] === 'number') {
+  //         copy[i + 3] += 1;
+  //       }
+  //       if (copy[i + 4] !== undefined && typeof copy[i + 4] === 'number') {
+  //         copy[i + 4] += 1;
+  //       }
+  //     }
+  //     if (i % 3 === 1) {
+  //       if (copy[i - 4] !== undefined && typeof copy[i - 4] === 'number') {
+  //         copy[i - 4] += 1;
+  //       }
+  //       if (copy[i - 3] !== undefined && typeof copy[i - 3] === 'number') {
+  //         copy[i - 3] += 1;
+  //       }
+  //       if (copy[i - 2] !== undefined && typeof copy[i - 2] === 'number') {
+  //         copy[i - 2] += 1;
+  //       }
+  //       if (copy[i - 1] !== undefined && typeof copy[i - 1] === 'number') {
+  //         copy[i - 1] += 1;
+  //       }
+  //       if (copy[i + 1] !== undefined && typeof copy[i + 1] === 'number') {
+  //         copy[i + 1] += 1;
+  //       }
+  //       if (copy[i + 2] !== undefined && typeof copy[i + 2] === 'number') {
+  //         copy[i + 2] += 1;
+  //       }
+  //       if (copy[i + 3] !== undefined && typeof copy[i + 3] === 'number') {
+  //         copy[i + 3] += 1;
+  //       }
+  //       if (copy[i + 4] !== undefined && typeof copy[i + 4] === 'number') {
+  //         copy[i + 4] += 1;
+  //       }
+  //     }
+  //     if (i % 3 === 2) {
+  //       if (copy[i - 4] !== undefined && typeof copy[i - 4] === 'number') {
+  //         copy[i - 4] += 1;
+  //       }
+  //       if (copy[i - 3] !== undefined && typeof copy[i - 3] === 'number') {
+  //         copy[i - 3] += 1;
+  //       }
+  //       if (copy[i - 1] !== undefined && typeof copy[i - 1] === 'number') {
+  //         copy[i - 1] += 1;
+  //       }
+  //       if (copy[i + 2] !== undefined && typeof copy[i + 2] === 'number') {
+  //         copy[i + 2] += 1;
+  //       }
+  //       if (copy[i + 3] !== undefined && typeof copy[i + 3] === 'number') {
+  //         copy[i + 3] += 1;
+  //       }
+  //     }
+  //   }
+  // }
+  // copy = copy.map((el) => {
+  //   if (el === true) {
+  //     return 1;
+  //   }
+  //   return el;
+  // });
+  // const res = [];
+  // for (let i = 0; i < copy.length; i += 3) {
+  //   res.push(copy.slice(i, i + 3));
+  // }
+  // return res;
 }
 
 module.exports = minesweeper;
