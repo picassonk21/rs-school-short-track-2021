afd930b611478180cb4bd4c16cc3115e82652f39
Egor Borisevich <picassonk21@gmail.com>
2021-04-06 10:39:51 +0300

    feat: add solution of 1-4 tasks

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,29 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+
+const sum = (arr, zeros) => {
+  let res = 0;
+  const zeroInd = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeroInd.push(i);
+    } else if (!zeros.includes(i)) {
+      res += arr[i];
+    }
+  }
+  return [res, zeroInd];
+};
+
+function getMatrixElementsSum(matrix) {
+  let result = 0;
+  let zeroInd = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let res = 0;
+    [res, zeroInd] = sum(matrix[i], zeroInd);
+    result += res;
+  }
+  return result;
 }
 
 module.exports = getMatrixElementsSum;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,23 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let heights = arr.filter((height) => height !== -1);
+  heights = heights.sort((a, b) => {
+    if (a > b) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+    return 0;
+  });
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      heights.splice(i, 0, -1);
+    }
+  }
+  return heights;
 }
 
 module.exports = sortByHeight;
